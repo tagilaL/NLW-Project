65d600956e86ba6de3acc8f9be4f560fbd79a696
Tagila Lima <tagila.lima96@gmail.com>
2020-06-28 15:40:23 -0300

    Corrigindo erro no select das cidades

--- public/scripts/create-point.js
+++ public/scripts/create-point.js
@@ -29,12 +29,15 @@ function getCities(event) {
 
     const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
 
+    citySelect.innerHTML = "<option value>Selecione a Cidade</option>"
+    citySelect.disabled = true
+
     fetch(url)
     .then( res => res.json())
     .then( cities => {
 
         for ( const city of cities ) {
-            citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+            citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
         }
 
         citySelect.disabled = false
@@ -42,9 +45,7 @@ function getCities(event) {
 }
 
 /* ouvidor de evento: clicar em alguma uf */
-document
-        .querySelector("select[name=uf]")
-        .addEventListener( "change", getCities )
+document.querySelector("select[name=uf]").addEventListener( "change", getCities )
 
 // Itens de coleta
 //pegar todos os li's
