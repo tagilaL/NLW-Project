272e3615184011ccad48be2b8df804fa32aa9de9
Tagila Lima <tagila.lima96@gmail.com>
2020-06-29 06:27:04 -0300

    Incluindo filtro de pesquisa por cidade

--- src/server.js
+++ src/server.js
@@ -71,8 +71,14 @@ server.post("/savepoint", (req, res) => {
 
 server.get("/search", (req, res) => {
 
+    const search = req.query.search
+
+    if(search == "") {
+        return res.render("search-results.html", { total: 0})
+    }
+
     //pegando os dados no banco
-    db.all(`SELECT * FROM places`, function(err, rows) {
+    db.all(`SELECT * FROM places WHERE city LIKE '%${search}%'`, function(err, rows) {
         if (err) {
             return console.log(err)
         }
